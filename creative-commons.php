c198340c537d4e12386895cfad63c7faaa94ff3d
Rob Myers <rob@robmyers.org>
2016-07-25 13:27:57 -0700

    Fix plugin details formatting.

--- creative-commons.php
+++ creative-commons.php
@@ -1,13 +1,9 @@
 <?php
 /*
 Plugin Name: Creative Commons
-Description: Official Creative Commons plugin for WordPress. Allows
-users to select and display Creative Commons licenses for their
-content. Partially inspired by the License plugin by mitcho (Michael
-Yoshitaka Erlewine) and Brett Mellor, as well as the original
-WpLicense plugin by CC CTO Nathan R. Yergler.
+Description: Official Creative Commons plugin for WordPress. Allows users to select and display Creative Commons licenses for their content. Partially inspired by the License plugin by mitcho (Michael Yoshitaka Erlewine) and Brett Mellor, as well as the original WpLicense plugin by CC CTO Nathan R. Yergler.
 Version: 2.0
-Author: Bjorn Wijers <burobjorn@burobjorn.nl>, Tarmo Toikkanen <tarmo@iki.fi>, Matt Lee <mattl@creativecommons.org>
+Author: Bjorn Wijers <burobjorn@burobjorn.nl>, Tarmo Toikkanen <tarmo@iki.fi>, Matt Lee <mattl@creativecommons.org>, Rob Myers <rob@creativecommons.org>
 Plugin URI: http://wiki.creativecommons.org/WpLicense
 License: GPLv2 or later versions
 */
