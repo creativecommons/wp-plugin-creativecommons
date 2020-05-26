b0184f003c57b0d2eee67c62e59391c1a75cdbd5
Hugo Solar <hugo.solar@gmail.com>
2020-05-26 10:17:38 -0400

    restore cc0_html_rdfa() function

--- includes/class-creativecommons.php
+++ includes/class-creativecommons.php
@@ -996,6 +996,41 @@ class CreativeCommons {
 		return $new_input;
 	}
 
+	/**
+	 * Function: cc0_html_rdfa
+	 *
+	 * @param  mixed $title_work
+	 * @param  mixed $attribute_url
+	 * @param  mixed $attribute_text
+	 */
+	public function cc0_html_rdfa( $title_work, $attribute_url, $attribute_text ) {
+		$result = '<p xmlns:dct="http://purl.org/dc/terms/" xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#">
+		<a rel="license"
+		href="http://creativecommons.org/publicdomain/zero/1.0/">
+		<img src="http://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0" /></a>
+		<br />
+		To the extent possible under law,';
+		if ( $attribute_url ) {
+			$result .= '
+				<a rel="dct:publisher"
+				href="' . $attribute_url . '">
+				<span property="dct:title">' . $attribute_text . '</span></a>';
+		} else {
+			$result .= '
+				<span resource="[_:publisher]" rel="dct:publisher">
+				<span property="dct:title">' . $attribute_text . '</span></span>';
+		}
+		$result .= '  has waived all copyright and related or neighboring rights to
+  		<span property="dct:title">' . $title_work . '</span>.'
+		/*
+		This work is published from:
+		<span property="vcard:Country" datatype="dct:ISO3166"
+		content="' . $country_code . '" about="' . $attribute_url . '">
+		' . $country_name . '</span>.
+		*/
+			. '</p>';
+		return $result;
+	}
 
 	/**
 	 * Print the Section text
