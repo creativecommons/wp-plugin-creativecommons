859973572824df0480de5f7a9c1dad2d7b818ab0
Hugo Solar <hugo.solar@gmail.com>
2020-05-26 13:46:58 -0400

    use local images & remove unnecessary comment

--- includes/class-creativecommons.php
+++ includes/class-creativecommons.php
@@ -1007,7 +1007,7 @@ class CreativeCommons {
 		$result = '<p xmlns:dct="http://purl.org/dc/terms/" xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#">
 		<a rel="license"
 		href="http://creativecommons.org/publicdomain/zero/1.0/">
-		<img src="http://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0" /></a>
+		<img src="'.CCPLUGIN__URL.'includes/images/cc0.png" style="border-style: none;" alt="CC0" /></a>
 		<br />
 		To the extent possible under law,';
 		if ( $attribute_url ) {
@@ -1022,12 +1022,6 @@ class CreativeCommons {
 		}
 		$result .= '  has waived all copyright and related or neighboring rights to
   		<span property="dct:title">' . $title_work . '</span>.'
-		/*
-		This work is published from:
-		<span property="vcard:Country" datatype="dct:ISO3166"
-		content="' . $country_code . '" about="' . $attribute_url . '">
-		' . $country_name . '</span>.
-		*/
 			. '</p>';
 		return $result;
 	}
