d58316b1f5484018f0a27ec799be4e3a7d2f2194
Abdul Wahab <meetawahab@gmail.com>
2020-10-31 22:22:43 +0500

    Enhancement: Make translatable string on creativecommons-widget.php

--- widgets/creativecommons-widget.php
+++ widgets/creativecommons-widget.php
@@ -94,10 +94,10 @@ if ( ! class_exists( 'CreativeCommons_Widget' ) ) {
 		public function form( $instance ) {
 			$title = ! empty( $instance['title'] ) ? $instance['title'] : ''; ?>
 			<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
-			<input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+				<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+				<input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
 			</p>
-			<p>Leave it empty to remove the title.</p>
+			<p><?php esc_html_e( 'Leave it empty to remove the title.', 'CreativeCommons'); ?></p>
 			<?php
 		}
 
